bf51eb05b3128eed5774cedd6ad399e535ccf8db
EdinsonHidalgo <edinsonjavierhidalgocifuentes@gmail.com>
2021-12-27 17:35:38 -0500

    Simulacion: Columns from file 'views.py'

--- src/components/home/components/data-filter.js
+++ src/components/home/components/data-filter.js
@@ -26,18 +26,70 @@ const DataFilter = () => {
 
     const fillHeaders = () => {
         const header = [
-            { label: 'FirstColumn', key: 'firstColumn' },
-            { label: 'SecondColumn', key: 'secondColumn' },
-            { label: 'ThirdColumn', key: 'thirdColumn' }
+            { label: 'ap_mac', key: 'ap_mac' },
+            { label: 'client_type', key: 'client_type' },
+            { label: 'ip', key: 'ip' },
+            { label: 'user_mac', key: 'user_mac' },
+            { label: 'band', key: 'band' },
+            { label: 'trafico_in', key: 'trafico_in' },
+            { label: 'trafico_out', key: 'trafico_out' },
+            { label: 'site', key: 'site' },
+            { label: 'site_id', key: 'site_id' },
+            { label: 'ap_group', key: 'ap_group' },
+            { label: 'nombre_sede', key: 'nombre_sede' },
+            { label: 'dda', key: 'dda' },
+            { label: 'estado_instalacion', key: 'estado_instalacion' },
+            { label: 'ubicacion', key: 'ubicacion' },
+            { label: 'cod_iso', key: 'cod_iso' },
+            { label: 'tipo_sitio_desc', key: 'tipo_sitio_desc' },
+            { label: 'grupo', key: 'grupo' },
+            { label: 'ip_wlan', key: 'ip_wlan' },
+            { label: 'mac_apoutdoor', key: 'mac_apoutdoor' },
+            { label: 'nombre_departamento', key: 'nombre_departamento' },
+            { label: 'municipio_pdet', key: 'municipio_pdet' },
+            { label: 'ip_wan', key: 'ip_wan' },
+            { label: 'banda_down', key: 'banda_down' },
+            { label: 'longitud', key: 'longitud' },
+            { label: 'cod_dane_depar', key: 'cod_dane_depar' },
+            { label: 'nombre_institucion_ed', key: 'nombre_institucion_ed' },
+            { label: 'cod_dane_sede', key: 'cod_dane_sede' },
+            { label: 'energia_desc', key: 'energia_desc' },
+            { label: 'cod_servicio', key: 'cod_servicio' },
+            { label: 'ip_prod_1', key: 'ip_prod_1' },
+            { label: 'detalle_sitio', key: 'detalle_sitio' },
+            { label: 'id_beneficiario', key: 'id_beneficiario' },
+            { label: 'ip_prod_2', key: 'ip_prod_2' },
+            { label: 'detalle_sitio_desc', key: 'detalle_sitio_desc' },
+            { label: 'latitud', key: 'latitud' },
+            { label: 'grupo_instalacion', key: 'grupo_instalacion' },
+            { label: 'energia', key: 'energia' },
+            { label: 'operador_cod', key: 'operador_cod' },
+            { label: 'cod_centro_poblado', key: 'cod_centro_poblado' },
+            { label: 'mac_2apoutdoor', key: 'mac_2apoutdoor' },
+            { label: 'mac_indoor', key: 'mac_indoor' },
+            { label: 'tipo_sitio', key: 'tipo_sitio' },
+            { label: 'cod_dane_muni', key: 'cod_dane_muni' },
+            { label: 'nombre_centro_pob', key: 'nombre_centro_pob' },
+            { label: 'banda_up', key: 'banda_up' },
+            { label: 'matricula', key: 'matricula' },
+            { label: 'grupo_desc', key: 'grupo_desc' },
+            { label: 'region', key: 'region' },
+            { label: 'nombre_municipio', key: 'nombre_municipio' },
+            { label: 'cod_dane_institucion_edu', key: 'cod_dane_institucion_edu' },
+            { label: 'timestamp', key: 'timestamp' },
+            { label: 'ancho_banda_carga', key: 'ancho_banda_carga' },
+            { label: 'ancho_banda_descarga', key: 'ancho_banda_descarga' },
+            { label: 'consumo', key: 'consumo' },
+            { label: 'concurrenciaConexiones', key: 'concurrenciaConexiones' }
         ];
         return header;
     }
 
     const fillData = () => {
         const data = [
-            { firstColumn: "firstData", secondColumn: "secondData", thirdColumn: "thirdData" },
-            { firstColumn: "firstData", secondColumn: "secondData", thirdColumn: "thirdData" },
-            { firstColumn: "firstData", secondColumn: "secondData", thirdColumn: "thirdData" }
+            { ap_mac: "exampleData1", client_type: "exampleData2", ip: "exampleData3", user_mac: "exampleData4", band: "exampleData5", trafico_in: "exampleData6", trafico_out: "exampleData7", site: "exampleData8", site_id: "exampleData9", ap_group: "exampleData10", nombre_sede: "exampleData11", dda: "exampleData12", estado_instalacion: "exampleData13", ubicacion: "exampleData14", cod_iso: "exampleData15", tipo_sitio_desc: "exampleData16", grupo: "exampleData17", ip_wlan: "exampleData18", mac_apoutdoor: "exampleData19", nombre_departamento: "exampleData20", municipio_pdet: "exampleData21", ip_wan: "exampleData22", banda_down: "exampleData23", longitud: "exampleData24", cod_dane_depar: "exampleData25", nombre_institucion_ed: "exampleData26", cod_dane_sede: "exampleData27", energia_desc: "exampleData28", cod_servicio: "exampleData29", ip_prod_1: "exampleData30", detalle_sitio: "exampleData31", id_beneficiario: "exampleData32", ip_prod_2: "exampleData33", detalle_sitio_desc: "exampleData34", latitud: "exampleData35", grupo_instalacion: "exampleData36", energia: "exampleData37", operador_cod: "exampleData38", cod_centro_poblado: "exampleData39", mac_2apoutdoor: "exampleData40", mac_indoor: "exampleData41", tipo_sitio: "exampleData42", cod_dane_muni: "exampleData43", nombre_centro_pob: "exampleData44", banda_up: "exampleData45", matricula: "exampleData46", grupo_desc: "exampleData47", region: "exampleData48", nombre_municipio: "exampleData49", cod_dane_institucion_edu: "exampleData50", timestamp: "exampleData51", ancho_banda_carga: "exampleData52", ancho_banda_descarga: "exampleData53", consumo: "exampleData54", concurrenciaConexiones: "exampleData55" },
+            { ap_mac: "exampleData1", client_type: "exampleData2", ip: "exampleData3", user_mac: "exampleData4", band: "exampleData5", trafico_in: "exampleData6", trafico_out: "exampleData7", site: "exampleData8", site_id: "exampleData9", ap_group: "exampleData10", nombre_sede: "exampleData11", dda: "exampleData12", estado_instalacion: "exampleData13", ubicacion: "exampleData14", cod_iso: "exampleData15", tipo_sitio_desc: "exampleData16", grupo: "exampleData17", ip_wlan: "exampleData18", mac_apoutdoor: "exampleData19", nombre_departamento: "exampleData20", municipio_pdet: "exampleData21", ip_wan: "exampleData22", banda_down: "exampleData23", longitud: "exampleData24", cod_dane_depar: "exampleData25", nombre_institucion_ed: "exampleData26", cod_dane_sede: "exampleData27", energia_desc: "exampleData28", cod_servicio: "exampleData29", ip_prod_1: "exampleData30", detalle_sitio: "exampleData31", id_beneficiario: "exampleData32", ip_prod_2: "exampleData33", detalle_sitio_desc: "exampleData34", latitud: "exampleData35", grupo_instalacion: "exampleData36", energia: "exampleData37", operador_cod: "exampleData38", cod_centro_poblado: "exampleData39", mac_2apoutdoor: "exampleData40", mac_indoor: "exampleData41", tipo_sitio: "exampleData42", cod_dane_muni: "exampleData43", nombre_centro_pob: "exampleData44", banda_up: "exampleData45", matricula: "exampleData46", grupo_desc: "exampleData47", region: "exampleData48", nombre_municipio: "exampleData49", cod_dane_institucion_edu: "exampleData50", timestamp: "exampleData51", ancho_banda_carga: "exampleData52", ancho_banda_descarga: "exampleData53", consumo: "exampleData54", concurrenciaConexiones: "exampleData55" },
+            { ap_mac: "exampleData1", client_type: "exampleData2", ip: "exampleData3", user_mac: "exampleData4", band: "exampleData5", trafico_in: "exampleData6", trafico_out: "exampleData7", site: "exampleData8", site_id: "exampleData9", ap_group: "exampleData10", nombre_sede: "exampleData11", dda: "exampleData12", estado_instalacion: "exampleData13", ubicacion: "exampleData14", cod_iso: "exampleData15", tipo_sitio_desc: "exampleData16", grupo: "exampleData17", ip_wlan: "exampleData18", mac_apoutdoor: "exampleData19", nombre_departamento: "exampleData20", municipio_pdet: "exampleData21", ip_wan: "exampleData22", banda_down: "exampleData23", longitud: "exampleData24", cod_dane_depar: "exampleData25", nombre_institucion_ed: "exampleData26", cod_dane_sede: "exampleData27", energia_desc: "exampleData28", cod_servicio: "exampleData29", ip_prod_1: "exampleData30", detalle_sitio: "exampleData31", id_beneficiario: "exampleData32", ip_prod_2: "exampleData33", detalle_sitio_desc: "exampleData34", latitud: "exampleData35", grupo_instalacion: "exampleData36", energia: "exampleData37", operador_cod: "exampleData38", cod_centro_poblado: "exampleData39", mac_2apoutdoor: "exampleData40", mac_indoor: "exampleData41", tipo_sitio: "exampleData42", cod_dane_muni: "exampleData43", nombre_centro_pob: "exampleData44", banda_up: "exampleData45", matricula: "exampleData46", grupo_desc: "exampleData47", region: "exampleData48", nombre_municipio: "exampleData49", cod_dane_institucion_edu: "exampleData50", timestamp: "exampleData51", ancho_banda_carga: "exampleData52", ancho_banda_descarga: "exampleData53", consumo: "exampleData54", concurrenciaConexiones: "exampleData55" }
         ];
         return data;
     }
--- src/components/list/table-component.js
+++ src/components/list/table-component.js
@@ -16,10 +16,62 @@ const TableComponent = ({ data, header }) => {
                     {data.map((element, index) => {
                         return (
                             <tr key={index}>
-                                <td>{index+1}</td>
-                                <td>{element.firstColumn}</td>
-                                <td>{element.secondColumn}</td>
-                                <td>{element.thirdColumn}</td>
+                                <td>{index + 1}</td>
+                                <td>{element.ap_mac}</td>
+                                <td>{element.client_type}</td>
+                                <td>{element.ip}</td>
+                                <td>{element.user_mac}</td>
+                                <td>{element.band}</td>
+                                <td>{element.trafico_in}</td>
+                                <td>{element.trafico_out}</td>
+                                <td>{element.site}</td>
+                                <td>{element.site_id}</td>
+                                <td>{element.ap_group}</td>
+                                <td>{element.nombre_sede}</td>
+                                <td>{element.dda}</td>
+                                <td>{element.estado_instalacion}</td>
+                                <td>{element.ubicacion}</td>
+                                <td>{element.cod_iso}</td>
+                                <td>{element.tipo_sitio_desc}</td>
+                                <td>{element.grupo}</td>
+                                <td>{element.ip_wlan}</td>
+                                <td>{element.mac_apoutdoor}</td>
+                                <td>{element.nombre_departamento}</td>
+                                <td>{element.municipio_pdet}</td>
+                                <td>{element.ip_wan}</td>
+                                <td>{element.banda_down}</td>
+                                <td>{element.longitud}</td>
+                                <td>{element.cod_dane_depar}</td>
+                                <td>{element.nombre_institucion_ed}</td>
+                                <td>{element.cod_dane_sede}</td>
+                                <td>{element.energia_desc}</td>
+                                <td>{element.cod_servicio}</td>
+                                <td>{element.ip_prod_1}</td>
+                                <td>{element.detalle_sitio}</td>
+                                <td>{element.id_beneficiario}</td>
+                                <td>{element.ip_prod_2}</td>
+                                <td>{element.detalle_sitio_desc}</td>
+                                <td>{element.latitud}</td>
+                                <td>{element.grupo_instalacion}</td>
+                                <td>{element.energia}</td>
+                                <td>{element.operador_cod}</td>
+                                <td>{element.cod_centro_poblado}</td>
+                                <td>{element.mac_2apoutdoor}</td>
+                                <td>{element.mac_indoor}</td>
+                                <td>{element.tipo_sitio}</td>
+                                <td>{element.cod_dane_muni}</td>
+                                <td>{element.nombre_centro_pob}</td>
+                                <td>{element.banda_up}</td>
+                                <td>{element.matricula}</td>
+                                <td>{element.grupo_desc}</td>
+                                <td>{element.region}</td>
+                                <td>{element.nombre_municipio}</td>
+                                <td>{element.cod_dane_institucion_edu}</td>
+                                <td>{element.timestamp}</td>
+                                <td>{element.ancho_banda_carga}</td>
+                                <td>{element.ancho_banda_descarga}</td>
+                                <td>{element.consumo}</td>
+                                <td>{element.concurrenciaConexiones}</td>
                             </tr>
                         );
                     })}
